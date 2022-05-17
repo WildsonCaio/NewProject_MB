b3cf7997bcdf74c1f5d855ade8f747e8291bd4c1
unknown <wcaio41@gmail.com>
2022-05-17 15:08:06 -0300

    update

--- PMB/views.py
+++ PMB/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Register_Employeers, Register_Services, Register_Time, Schedule
 
 def index(request):
-    return render(request, 'pages/index.html')
+    employeers = Register_Employeers.objects.all()
+    services = Register_Services.objects.all()
+    time = Register_Time.objects.all()
+    return render(request, 'pages/index.html', {'employeers':employeers, 'services':services, 'time':time})
