5b06517e66e11efd874e4941666f39cdf199bdad
unknown <wcaio41@gmail.com>
2022-05-23 16:40:41 -0300

    update

--- PMB/views.py
+++ PMB/views.py
@@ -1,3 +1,5 @@
+from itertools import product
+from unicodedata import name
 from django.shortcuts import render, redirect
 from .models import Register_Employeers, Register_Services, Register_Time, Schedule
 
@@ -7,8 +9,6 @@ def agenda(request):
     time = Register_Time.objects.all()
     return render(request, 'pages/index.html', {'employeers':employeers, 'services':services, 'time':time})
 
-
-#CORRIGIR BUG AMANHÃ
 def add_employeer(request):
     if request.method == "POST":
         name = request.POST.get('name')
@@ -16,15 +16,31 @@ def add_employeer(request):
         image = request.FILES.get('image')
         created = Register_Employeers.objects.create(user_id=request.user.id, name=name, phone=phone, photo=image)
         created.save()
-        return redirect('add_employeer', {'employeer':employeer})
+        return redirect('add_employeer')
         
         
     else:    
         employeer = Register_Employeers.objects.all()
         return render(request, 'pages/add_employeer.html',{'employeer':employeer})
 
-def add_service(request):
-    return render(request, 'pages/add_service.html')
+def add_service(request):    
+    #t = Register_Services.objects.get(user_id=1)
+    #print(t.professionals.all())
+    if request.method == "POST":
+        employeers = Register_Employeers.objects.all()
+        name = request.POST.get('name')
+        technique = request.POST.get('technique')
+        products = request.POST.get('product')
+        professionals = request.POST.getlist('professionals')
+        for i in professionals:
+            professional = Register_Employeers.objects.get(name=i)
+            add = Register_Services.objects.create(user_id=request.user.id, name=name, technique=technique,
+                                               products=products, professionals=professional)
+            add.save()
+        return redirect('add_service')
+    else:
+        employeers = Register_Employeers.objects.all()
+        return render(request, 'pages/add_services.html', {'employeers':employeers})
 
 def add_time(request):
     return render(request, 'pages/add_time.html')
